197c26f34cdb5e058608bf2e2070bec73907f44f
sfz1wdorf <106619917+sfz1wdorf@users.noreply.github.com>
2022-06-07 22:52:50 +0200

    Added support for single-line keys

--- encrypt.rb
+++ encrypt.rb
@@ -1,17 +1,14 @@
-# queries N
-puts "N: "
-n = gets.to_i
-
-# queries E
-puts "E: "
-e = gets.to_i
-
-# queries M
+require_relative 'cantor'
+#pulls the cantor function from the cantor.rb file
+#asks for public key
+puts "öffentlicher Schlüssel:"
+L = gets.to_i
+#asks for M
 puts "Note:"
 M = gets.to_f
-
 # processes decimal numbers
 x = (M*100).to_i
+e, n = inv_cantor(L)
 
 # calculates C (C = M^e  mod  N)
 C = x**e % n
