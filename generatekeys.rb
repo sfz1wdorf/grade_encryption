1a1aff0368b67cc7e83911ac00b8ae430619827b
CoronixTV <64707606+CoronixTV@users.noreply.github.com>
2022-06-07 18:34:50 +0200

    Added Comments

--- generatekeys.rb
+++ generatekeys.rb
@@ -1,10 +1,11 @@
 
 require "prime"
 iscoprime = false
-
+#Generates a random Prime-number
+#between the 11th and 18th prime
 p = Prime.first(rand(11..18)).last
 q = Prime.first(rand(11..18)).last
-
+#function to check if two numbers are coprime
 def coprime?(a, b)
     a.gcd(b) == 1
     iscoprime = true
@@ -16,14 +17,17 @@ end
 
 n = p * q
 m = (p - 1) * (q - 1)
-
+#generates Prime e and checks if it's
+#coprime to m
 e = Prime.first(rand(5..10)).last
 coprime?(e, m)
+#if not, generate it again.
 while iscoprime == true
     e = Prime.first(rand(5..10)).last
     coprime?(e, m)
 end
-
+#function using the extended Euclidean
+#algorythm to determine d
 def extended_euclidean(a, b)
     if b == 0
         return [1, 0]
@@ -32,10 +36,12 @@ def extended_euclidean(a, b)
         return [y, x - (a / b) * y]
     end
 end
+#checks if d is coprime to m or d < 1
 d = extended_euclidean(e, m)[0]
 if d < 0 or coprime?(d, m) == true
     d += m
 end
+#outputs generated keys
 puts "Privater Schlüssel    :"
 puts "  D: #{d}"
 puts "  N: #{n}"
