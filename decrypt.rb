4152988ab3218af97b8b880220f351bc21fc9171
CoronixTV <64707606+CoronixTV@users.noreply.github.com>
2022-06-13 23:32:34 +0200

    Bug fix

--- decrypt.rb
+++ decrypt.rb
@@ -3,13 +3,16 @@ require_relative 'cantor'
 puts "privater Schlüssel:"
 L = gets.to_i
 d, n = inv_cantor(L)
-
+puts n
+puts d
 # pulls C
 puts "Verschlüsselte Note:"
 C = gets.to_i
 
 # calculates M (M = C^d  mod  N)
-M = C**d % n
+f = C % n
+
+M = f**d % n
 
 # outputs M
 puts "Deine Note: #{M.to_f/100}"
